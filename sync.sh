61126b40d2323b98c3cc15c6efdccf6b829cad28
Pongsak Suvanpong <psksvp@gmail.com>
2016-10-06 17:24:56 +1100

    remove CodeEditor.scala
    so don’t need to more dependency than I need to

--- sync.sh
+++ sync.sh
@@ -3,5 +3,6 @@ mkdir -p src/main/scala/psksvp
 mkdir -p src/main/resources/psksvp
 cp -R ../CodeWithJVM/src/psksvp/AbstractStateMachine src/main/scala/psksvp/. 
 cp -R ../CodeWithJVM/src/psksvp/GUI src/main/scala/psksvp/.
+rm src/main/scala/psksvp/GUI/CodeEditor.scala
 cp -R ../CodeWithJVM/src/psksvp/Terminal src/main/scala/psksvp/.
 cp ../CodeWithJVM/src/psksvp/package.scala src/main/scala/psksvp/.
